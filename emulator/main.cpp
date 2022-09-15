c2890278bce4b8631a44e51b6635508705b3980c
colinpaint <88828347+colinpaint@users.noreply.github.com>
2022-09-15 12:26:52 +0100

    inch you way towards it

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -4,10 +4,6 @@
   #include <windows.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include <array>
 #include <vector>
 #include <string>
@@ -60,13 +56,113 @@ int check (enum sp_return result)
 }
 //}}}
 
-vector <string> portNames;
+constexpr bool kNtsc = false;
+//{{{
+class cRamcorderMaster {
+public:
+  cRamcorderMaster() {}
+  ~cRamcorderMaster() {}
+
+  //{{{
+  void init() {
+
+    mStatus = 0;
+
+    if (kNtsc)
+      mClipLength = 383;
+    else
+      mClipLength = 323;
+
+    mTimecode [0] = 0;
+    mTimecode[1] = 0;
+    mTimecode[2] = 0;
+    mTimecode[3] = 0;
+
+    mFieldNum = 0;
+  }
+  //}}}
+
+  //{{{
+  uint16_t getSize() {
+    return mClipLength;
+  }
+  //}}}
+
+  //{{{
+  bool selectProtocol (uint8_t protocol) {
+    return true;
+  }
+  //}}}
+  //{{{
+  bool position (bool waitForReply, uint16_t field, bool fieldMode, bool field2Dom, bool reverse) {
+    return true;
+  }
+  //}}}
+  //{{{
+  bool selectClip (bool inputClip, bool fieldMode, bool field2Dom,
+                   uint16_t startField, uint16_t finishField,
+                   bool play_reverse, bool play_bounce,
+                   bool rec_loop, bool play_loop,
+                   bool cine_expand, bool cine_compress) {
+    return true;
+  }
+  //}}}
+  //{{{
+  bool pollField (uint16_t field, bool field2dom) {
+    return false;
+  }
+  //}}}
+  //{{{
+  bool go (bool playGo, bool recordGo, uint16_t goDelay) {
+    return true;
+  }
+  //}}}
+  //{{{
+  bool stop() {
+    return true;
+  }
+  //}}}
+
+private:
+  //{{{
+  void startPacket (char command) {
+  }
+  //}}}
+  //{{{
+  void addUint8 (uint8_t value) {
+  }
+  //}}}
+  //{{{
+  void addChar (char c) {
+  }
+  //}}}
+  //{{{
+  void addWord (int16_t value) {
+  }
+  //}}}
+
+  //{{{
+  bool sendCommand (bool waitForReply) {
+    return true;
+  }
+  //}}}
+
+  uint8_t mStatus = 0;
+  array <uint8_t,4> mTimecode = {0};
+  uint16_t mFieldNum = 0;
+  uint16_t mClipLength = 0;
+
+  array <uint8_t, 16> mPacket = {0};
+};
+//}}}
 
 int main (int numArgs, char** args)
 {
-  eLogLevel logLevel = LOGINFO;
+  vector <string> portNames;
+
   string usePortName;
-  //{{{  parse params
+  eLogLevel logLevel = LOGINFO;
+  //{{{  parse commandLine params
   for (int i = 1; i < numArgs; i++) {
     string param = args[i];
     if (param == "log1")
@@ -83,9 +179,8 @@ int main (int numArgs, char** args)
   cLog::init (logLevel);
   cLog::log (LOGNOTICE, fmt::format ("ramcorder emulator - serial port test"));
 
-  //{{{  get portList
+  //{{{  get portList, portnames
   struct sp_port** portList;
-
   enum sp_return result = sp_list_ports (&portList);
   if (result != SP_OK) {
     cLog::log (LOGINFO, fmt::format ("sp_list_ports failed"));
@@ -94,8 +189,8 @@ int main (int numArgs, char** args)
     #endif
     return -1;
   }
-  //}}}
-  //{{{  get portNames
+
+  // get portNames
   for (int i = 0; portList[i] != NULL; i++)
     portNames.push_back (sp_get_port_name (portList[i]));
 
@@ -131,7 +226,7 @@ int main (int numArgs, char** args)
   check (sp_set_flowcontrol (port, SP_FLOWCONTROL_NONE));
   //}}}
 
-  //{{{  tx on port
+  //{{{  tx port
   const char* data = "Hello - this is some looped back data! 1234567890";
   size_t size = strlen (data);
 
@@ -147,7 +242,7 @@ int main (int numArgs, char** args)
   // about to receive the data on port
   cLog::log (LOGINFO, fmt::format ("about to rx {}:bytes on port:{}", size, sp_get_port_name (port)));
 
-  //{{{  rx from port
+  //{{{  rx port
   // allocate buffer to receive data, 1 second timeout
   char* buf = (char*)malloc (size + 1);
   int bytesRead = check (sp_blocking_read (port, buf, size, timeout));
@@ -164,7 +259,7 @@ int main (int numArgs, char** args)
   free (buf);
   //}}}
 
-  //{{{  close and free port
+  //{{{  close,free port
   check (sp_close (port));
   sp_free_port (port);
   //}}}
